043b959a8ef9184d6548521ccb968f22e54192db
Fabian Kjellberg <Fabian.Kjellberg98@gmail.com>
2026-01-28 15:55:50 +0100

    connected to DB and BLOB
    connected to DB and BLOB

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,20 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/api/test', (c) => {
-
-  const result = c.env.DB.prepare('SELECT * FROM users').all();
-  return c.json({ message: 'Hello Hono!', result })
+app.get('/api/test', async (c) => {
+  try {
+    const result = await c.env.DB
+      .prepare('SELECT * FROM users')
+      .all()
+    console.log('Query result:', result)
+    return c.json({
+      message: 'Hello Hono!',
+      result
+    })
+  } catch (error) {
+    console.error('DB error:', error)
+    return c.text('Internal Server Error', 500)
+  }
 })
 
 export default app
